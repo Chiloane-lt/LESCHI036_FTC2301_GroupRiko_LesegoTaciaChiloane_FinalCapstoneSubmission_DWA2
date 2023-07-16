04199c5f3fdff9341c1793b0099d90327f58fa8a
Chiloane-lt <chiloane.lt@hotmail.com>
2023-07-16 23:22:22 +0200

    Create NavBar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import Logo from './assets/talkbox-inverted.svg'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import NavBar from './components/NavBar'
+
+
 
 function App() {
   return (
-    <><nav className='w-16 flex'>
-      <img src={Logo} className='w-4/5'></img>
-      <FontAwesomeIcon icon={faBars}/>
-    </nav>
+    <>
+      <NavBar />
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faCircleUser } from "@fortawesome/free-solid-svg-icons";
+
+export default function NavBar() {
+  return (
+    <header
+    className="h-16 w-full fixed bg-dark-green bg-opacity-50 backdrop-blur-sm">
+      <nav className="flex justify-between px-4 mt-4 items-center">
+        <h1 className="text-mint-cream">
+          Talk
+          <span className="font-bold">BOX</span>
+        </h1>
+        <div className="w-1/4 flex justify-between">
+        <FontAwesomeIcon icon={faHeart} className="h-6 min-[300px]:h-8 text-mint-cream"/>
+        <FontAwesomeIcon icon={faCircleUser} className="h-6 min-[300px]:h-8 text-mint-cream"/>
+        </div>
+      </nav>
+    </header>
+  )
+}
