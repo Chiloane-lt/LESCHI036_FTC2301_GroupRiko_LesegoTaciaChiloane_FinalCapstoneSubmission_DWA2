2b6042275b5a8fa7e0d42bb5d8c0c357e807b4b7
Chiloane-lt <chiloane.lt@hotmail.com>
2023-07-16 23:56:00 +0200

    Improve Hero accessibility.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <div className="bg-[url('https://cdn.pixabay.com/photo/2016/11/29/04/42/conifers-1867371_1280.jpg')] w-full h-[35vh] bg-cover bg-no-repeat flex items-end overflow-hidden">
+    <section className="bg-[url('https://cdn.pixabay.com/photo/2016/11/29/04/42/conifers-1867371_1280.jpg')] w-full h-[35vh] bg-cover bg-no-repeat flex items-end overflow-hidden">
       <h1 className="text-mint-cream text-dynamic p-2">
         Not just podcasts...<br/>Your daily dose of fresh thoughts.
       </h1>
-    </div>
+    </section>
   )
 }
