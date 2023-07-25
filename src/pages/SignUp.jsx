ce5687aba3265b7b970329b28197e794070dcc91
Chiloane-lt <chiloane.lt@hotmail.com>
2023-07-25 03:16:25 +0200

    Update signup process.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom"
 import SignUpForm from "../components/SignUpForm"
 
-export const Signup = () => {
+const SignUp = () => {
   return (
     <main className="bg-[url('https://cdn.pixabay.com/photo/2020/06/07/02/16/fantasy-5268744_960_720.jpg')] bg-center bg-no-repeat h-screen w-screen text-mint-cream">
       <h1 className="text-center">Talk<span>BOX</span></h1>
       <SignUpForm />
+      <Link to={'/signin'}
+      className="underline">
+        Already have an account?
+      </Link>
     </main>
     )
 }
+
+export default SignUp
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -7,7 +7,6 @@ export default function User() {
     <>
       <NavBar />
       <Hero />
-      <SignUpForm />
     </>
     )
 }
