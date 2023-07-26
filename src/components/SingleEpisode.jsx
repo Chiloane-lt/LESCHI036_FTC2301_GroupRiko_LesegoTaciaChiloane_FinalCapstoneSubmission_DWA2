2f7808aa8690cfe9d6d720fe4b2eafca1e52ad3a
Chiloane-lt <chiloane.lt@hotmail.com>
2023-07-26 11:29:00 +0200

    Tudy up logic.

--- src/components/SingleEpisode.jsx
+++ src/components/SingleEpisode.jsx
@@ -15,11 +15,14 @@ function SingleEpisode(props) {
 
   // Get duration
   useEffect(() => {
-    const minutes = !ref.current.duration ? 0 : Math.floor(ref.current.duration / 60);
-    const seconds = !ref.current.duration ? 0 : Math.floor(ref.current.duration);
-    const time = `${minutes > 9 ? minutes : ('0' + minutes)}:${seconds > 9 ? seconds : ('0' + seconds)}`    
-    setDuration(time)
-  }, [ref?.current?.loadedmetadata])
+    setDuration(ref.current.duration)
+  }, [ref?.current?.loadedmetadata, ref?.current?.readyState])
+
+  const getTimeStamp = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time%60);
+    return `${minutes > 9 ? minutes : ('0' + minutes)}:${seconds > 9 ? seconds : ('0' + seconds)}`
+  }
 
   const toggleDropdown = () => {
     setIsDescriptionOpen((prev) => !prev)
@@ -31,7 +34,7 @@ function SingleEpisode(props) {
       <FontAwesomeIcon icon={faCirclePlay} className="w-[15%] text-2xl"/>
       <div className="w-[70%]">
         <h1 className="font-medium">{episode.title}</h1>
-        <p className="font-light">Episode {episode.episode} • <FavouriteButton /> • {duration}</p>
+        <p className="font-light">Episode {episode.episode} • <FavouriteButton /> • {duration ? getTimeStamp(duration) : '00:00'}</p>
       </div>
       {isDescriptionOpen ? (
             <FontAwesomeIcon icon={faAngleUp} onClick={toggleDropdown} className="w-[15%] text-xl"/>) : (
@@ -40,7 +43,7 @@ function SingleEpisode(props) {
     </div>
     {isDescriptionOpen && <p className="text-xs px-8 pb-4 text-justify">{episode.description}</p>}
     <audio
-    preload
+    preload='metadata'
     ref={ref}>
       <source src={episode.file} type="audio/ogg" />
     </audio>
