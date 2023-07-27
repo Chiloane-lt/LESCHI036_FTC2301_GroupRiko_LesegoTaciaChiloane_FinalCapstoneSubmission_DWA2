b55ca8451693da36ffce5eec3f98cdf90179dbc9
Chiloane-lt <chiloane.lt@hotmail.com>
2023-07-27 12:19:37 +0200

    Create user settings modal.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,35 @@
-import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart, faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { useState } from 'react'
+
+import { NavLink } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHeart, faCircleUser } from "@fortawesome/free-solid-svg-icons"
+import UserSettings from "./UserSettings"
 
 export default function NavBar() {
+
+  const [ isOpen, setIsOpen ] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(prev => !prev);
+  }
+
   return (
-    <header
-    className="h-16 w-full fixed bg-dark-green bg-opacity-50 backdrop-blur-sm">
+    <header className="h-16 w-full fixed bg-dark-green bg-opacity-50 backdrop-blur-sm">
       <nav className="flex justify-between px-4 mt-4 items-center">
-        <NavLink to={'/home'} className="text-mint-cream">
+        <NavLink to={"/home"} className="text-mint-cream">
           Talk<span className="font-bold">BOX</span>
         </NavLink>
+        
         <div className="w-1/4 flex justify-around">
-          <NavLink to={'/home/favourites'} >
-            <FontAwesomeIcon icon={faHeart} className="h-4 min-[300px]:h-6 text-mint-cream"/>
+          <NavLink to={"/home/favourites"}>
+            <FontAwesomeIcon icon={faHeart} className="h-4 min-[300px]:h-6 text-mint-cream" />
           </NavLink>
-          <NavLink to={'/home/user'} >
-            <FontAwesomeIcon icon={faCircleUser} className="h-4 min-[300px]:h-6 text-mint-cream"/>
-          </NavLink>
+          <button
+          onClick={toggleModal}
+          className="active:animate-ping">
+            <FontAwesomeIcon icon={faCircleUser} className="h-4 min-[300px]:h-6 text-mint-cream" />
+          </button>
+          <UserSettings active={isOpen} setter={toggleModal}/>
         </div>
       </nav>
     </header>
