f2bc5bccdf5b40bc9ce83136407dc7dfba501496
Chiloane-lt <chiloane.lt@hotmail.com>
2023-07-29 08:50:40 +0200

    Update favourites data fetching.

--- src/components/FavouriteButton.jsx
+++ src/components/FavouriteButton.jsx
@@ -1,21 +1,31 @@
-import React, { useState } from 'react'
+import { useState, useEffect } from "react"
 
 // icons
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-import { faHeart as FaHeart } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHeart } from "@fortawesome/free-solid-svg-icons"
+import { faHeart as FaHeart } from "@fortawesome/free-regular-svg-icons"
 
-function FavouriteButton() {
-  const [ isFavourite, setIsFavourite ] = useState(false);
+function FavouriteButton(props) {
+  const { state } = props
+  const [isFavourite, setIsFavourite] = useState(state)
+
+  useEffect(() => {
+    setIsFavourite(state)
+  }, [state])
 
   const toggleFavourite = () => {
-    setIsFavourite(prev => !prev);
+    setIsFavourite((prev) => {
+      console.log(prev)
+      return !prev
+    })
   }
   return (
     <>
-    {isFavourite ? <FontAwesomeIcon icon={faHeart} onClick={toggleFavourite} className='active:animate-ping'/> : <FontAwesomeIcon icon={FaHeart} onClick={toggleFavourite} className='active:animate-ping'/>}
-      
-      
+      {isFavourite ? (
+        <FontAwesomeIcon icon={faHeart} onClick={toggleFavourite} className="active:animate-ping" />
+      ) : (
+        <FontAwesomeIcon icon={FaHeart} onClick={toggleFavourite} className="active:animate-ping" />
+      )}
     </>
   )
 }
--- src/components/SingleEpisode.jsx
+++ src/components/SingleEpisode.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState, useEffect } from "react"
+import { supabase } from "../supabaseClient"
 
 // icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -6,47 +7,51 @@ import { faCirclePlay, faAngleDown, faAngleUp } from "@fortawesome/free-solid-sv
 
 // components
 import FavouriteButton from "./FavouriteButton"
+import useFavouriteCheck from "../hooks/useFavouriteCheck"
 
 function SingleEpisode(props) {
   const { episode } = props
-  const [isDescriptionOpen, setIsDescriptionOpen] = useState(false);
-  const [ duration, setDuration ] = useState('00:00');
-  const ref = useRef(0)
 
   // Get duration
+  const [duration, setDuration] = useState("00:00")
+  const ref = useRef(0)
   useEffect(() => {
     setDuration(ref.current.duration)
   }, [ref?.current?.loadedmetadata, ref?.current?.readyState])
 
   const getTimeStamp = (time) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = Math.floor(time%60);
-    return `${minutes > 9 ? minutes : ('0' + minutes)}:${seconds > 9 ? seconds : ('0' + seconds)}`
+    const minutes = Math.floor(time / 60)
+    const seconds = Math.floor(time % 60)
+    return `${minutes > 9 ? minutes : "0" + minutes}:${seconds > 9 ? seconds : "0" + seconds}`
   }
 
+  // Description dropdown
+  const [isDescriptionOpen, setIsDescriptionOpen] = useState(false)
   const toggleDropdown = () => {
     setIsDescriptionOpen((prev) => !prev)
   }
 
+  // handle favourites
+  const isFavourite = useFavouriteCheck(`${episode.title}`)
+
   return (
     <div className="flex flex-col bg-mint-cream text-dark-green shadow-md">
-    <div className="h-20 flex items-center text-sm">
-      <FontAwesomeIcon icon={faCirclePlay} className="w-[15%] text-2xl"/>
-      <div className="w-[70%]">
-        <h1 className="font-medium">{episode.title}</h1>
-        <p className="font-light">Episode {episode.episode} • <FavouriteButton /> • {duration ? getTimeStamp(duration) : '00:00'}</p>
+      <div className="h-20 flex items-center text-sm">
+        <FontAwesomeIcon icon={faCirclePlay} className="w-[15%] text-2xl" />
+        <div className="w-[70%]">
+          <h1 className="font-medium">{episode.title}</h1>
+          <p className="font-light">
+            Episode {episode.episode} • <FavouriteButton state={isFavourite} /> •{" "}
+            {duration ? getTimeStamp(duration) : "00:00"}
+          </p>
+        </div>
+        {isDescriptionOpen ? (
+          <FontAwesomeIcon icon={faAngleUp} onClick={toggleDropdown} className="w-[15%] text-xl" />
+        ) : (
+          <FontAwesomeIcon icon={faAngleDown} onClick={toggleDropdown} className="w-[15%] text-xl" />
+        )}
       </div>
-      {isDescriptionOpen ? (
-            <FontAwesomeIcon icon={faAngleUp} onClick={toggleDropdown} className="w-[15%] text-xl"/>) : (
-            <FontAwesomeIcon icon={faAngleDown} onClick={toggleDropdown} className="w-[15%] text-xl"/>
-          )}
-    </div>
-    {isDescriptionOpen && <p className="text-xs px-8 pb-4 text-justify">{episode.description}</p>}
-    <audio
-    preload='metadata'
-    ref={ref}>
-      <source src={episode.file} type="audio/ogg" />
-    </audio>
+      {isDescriptionOpen && <p className="text-xs px-8 pb-4 text-justify">{episode.description}</p>}
     </div>
   )
 }
