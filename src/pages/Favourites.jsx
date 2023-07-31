5ac98968802e439e50fc0b57b894a6b67de253e1
Chiloane-lt <chiloane.lt@hotmail.com>
2023-07-31 08:25:45 +0200

    Update episode card.

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -3,65 +3,54 @@ import { supabase } from "../supabaseClient"
 
 import NavBar from "../components/NavBar"
 import Hero from "../components/Hero"
+import FavouritesCards from "../components/FavouritesCards"
 
 const Favourites = () => {
-  const [ favourites, setFavourites ] = useState([])
-  const [ isLoading, setIsLoading ] = useState(true)
-  const groupContainer = [];
+  const [favourites, setFavourites] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const groupedContainers = []
 
   useEffect(() => {
     const fetchFaves = async () => {
-      let { data, error } = await supabase
-      .from("favourites")
-      .select("*")
-      setFavourites(data)
-      setIsLoading(false)
+      try {
+        let { data, error } = await supabase.from("favourites").select("*")
+        if (error) {
+          throw new Error("Failed to fetch favourites")
+        }
+        setFavourites(data)
+        setIsLoading(false)
+      } catch (error) {
+        console.error(error)
+        setIsLoading(false)
+      }
     }
     fetchFaves()
   }, [])
-
   const grouped = favourites.reduce(function (r, a) {
-    r[a.show] = r[a.show] || [];
-    r[a.show].push(a);
-    return r;
-}, Object.create(null));
+    r[a.show] = r[a.show] || []
+    r[a.show].push(a)
+    return r
+  }, Object.create(null))
 
-
-if(grouped) {
-//   const favesContainer = grouped.map((show, index) => {
-//   return <fieldset key={index}></fieldset>
-// })
-
-
-
-for (const [key, value] of Object.entries(grouped)) {
-  groupContainer.push(
-  <fieldset key={key}
-  className="border border-black m-4">
-    <legend>{key}</legend>
-    {value.map((episode) => {
-      return (
-        <div key={episode.id}>
-          <h1>{episode.title}</h1>
+  if (grouped) {
+    for (const [key, value] of Object.entries(grouped)) {
+      groupedContainers.push(
+        <div key={key} className="border border-black m-4">
+          {value.map((episode) => {
+            return <FavouritesCards key={episode.id} data={episode} />
+          })}
         </div>
       )
-    })}
-  </fieldset>
-  )
-}}
-
-
-
-// Iterate through groups
-// For each return a container
-// Pass group array to container
+    }
+  }
 
   return (
     <>
       <NavBar />
       <Hero />
       {isLoading && <h1>Loading...</h1>}
-      {!isLoading && <div>{groupContainer}</div>}
+      {!isLoading && favourites.length === 0 && <h1>No favourites found.</h1>}
+      {!isLoading && groupedContainers}
     </>
   )
 }
